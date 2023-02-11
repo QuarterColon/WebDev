7b555945dc3b500e0c4e426c7c832dea14e2359a
Ankit Chakraborty <ankitchakraborty1972@gmail.com>
2023-02-12 02:22:30 +0530

    CHanged Image

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -4,30 +4,32 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 function PageTransitions(){
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function() {
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn'
+        [...document.querySelectorAll(".control")].forEach(button => {
+            button.addEventListener("click", function() {
+                document.querySelector(".active-btn").classList.remove("active-btn");
+                this.classList.add("active-btn");
+                document.querySelector(".active").classList.remove("active");
+                document.getElementById(button.dataset.id).classList.add("active");
+            })
         })
-    }
+        
 
-    allSections.addEventListener('click', (e) =>{
-        const id = e.target.dataset.id;
-        if(id){
-            sectBtns.forEach((btn) =>{
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
+    // allSections.addEventListener('click', (e) =>{
+    //     const id = e.target.dataset.id;
+    //     if(id){
+    //         sectBtns.forEach((btn) =>{
+    //             btn.classList.remove('active')
+    //         })
+    //         e.target.classList.add('active')
 
-            sections.forEach((section)=>{
-                section.classList.remove('active')
-            })
+    //         sections.forEach((section)=>{
+    //             section.classList.remove('active')
+    //         })
 
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-    })
+    //         const element = document.getElementById(id);
+    //         element.classList.add('active');
+    //     }
+    // })
 
     const themeBtn = document.querySelector('.theme-button');
     themeBtn.addEventListener('click', () => {
